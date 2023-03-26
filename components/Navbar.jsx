3707d75aa082fe5145cf68652327513688679792
Jyotirmoy <jyotirmoykarmakar02@gmail.com>
2023-03-26 18:31:45 +0530

    routing & user store

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,18 @@
 import React, { useContext, useEffect, useState } from "react";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 import { apeicon } from "@/assets";
 import ApeContext from "@/context/ApeContext";
 import { shortenAddress } from "@/utils/shortenAddr";
 
 const Navbar = () => {
-  const { connectWallet, walletAddress } = useContext(ApeContext);
+  const { connectWallet, walletAddress, currentPage, setCurrentPage } =
+    useContext(ApeContext);
+
   const [addrval, setAddrval] = useState("");
+
+  const navigate = useRouter();
 
   useEffect(() => {
     setAddrval(walletAddress);
@@ -35,7 +40,7 @@ const Navbar = () => {
           <div className='flex md:order-2'>
             <button
               type='button'
-              className='text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:outline-none shadow-lg dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center my-[5px] w-[160px] '
+              className='text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:outline-none shadow-lg dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-md px-5 py-2.5 text-center my-[5px] w-[160px] '
               onClick={() => connectWallet()}
             >
               {walletAddress ? (
@@ -46,46 +51,53 @@ const Navbar = () => {
             </button>
           </div>
 
-          {/* <div
-            className='items-center justify-between hidden w-full md:flex md:w-auto md:order-1'
-            id='navbar-sticky'
-          >
-            <ul className='flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
+          <div className='items-center justify-between hidden w-full md:flex md:w-auto md:order-1'>
+            <ul className='flex p-4 mt-4 border border-gray-100 rounded-lg flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 dark:border-gray-700'>
               <li>
-                <a
-                  href='#'
-                  className='block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white'
-                  aria-current='page'
+                <button
+                  className={`block py-2 pl-3 pr-4  rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 text-[16px] ${
+                    currentPage === "home" ? "text-blue-500" : "text-gray-400"
+                  }`}
+                  onClick={() => {
+                    setCurrentPage("home");
+                    navigate.push("/");
+                  }}
                 >
                   Home
-                </a>
+                </button>
               </li>
+
               <li>
-                <a
-                  href='#'
-                  className='block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
+                <button
+                  className={`block py-2 pl-3 pr-4  rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 text-[16px] ${
+                    currentPage === "events" ? "text-blue-500" : "text-gray-400"
+                  }`}
+                  onClick={() => {
+                    setCurrentPage("events");
+                    navigate.push("/events");
+                  }}
                 >
-                  About
-                </a>
+                  Events
+                </button>
               </li>
+
               <li>
-                <a
-                  href='#'
-                  className='block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
+                <button
+                  className={`block py-2 pl-3 pr-4  rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 text-[16px] ${
+                    currentPage === "governance"
+                      ? "text-blue-500"
+                      : "text-gray-400"
+                  }`}
+                  onClick={() => {
+                    setCurrentPage("governance");
+                    navigate.push("/governance");
+                  }}
                 >
-                  Services
-                </a>
-              </li>
-              <li>
-                <a
-                  href='#'
-                  className='block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
-                >
-                  Contact
-                </a>
+                  Governance
+                </button>
               </li>
             </ul>
-          </div> */}
+          </div>
         </div>
       </nav>
     </div>
--- context/ApeContext.js
+++ context/ApeContext.js
@@ -5,6 +5,11 @@ const ApeContext = createContext({});
 export const ApeProvider = ({ children }) => {
   const [walletAddress, setWalletAddress] = useState("");
   const [isModal, setIsModal] = useState(false);
+  const [currentPage, setCurrentPage] = useState("home");
+  const [inputFields, setInputFields] = useState({
+    name: "",
+    username: "",
+  });
 
   const connectWallet = async () => {
     if (!window.ethereum) return alert("Please install MetaMask.");
@@ -25,6 +30,10 @@ export const ApeProvider = ({ children }) => {
         connectWallet,
         isModal,
         setIsModal,
+        currentPage,
+        setCurrentPage,
+        inputFields,
+        setInputFields,
       }}
     >
       {children}
