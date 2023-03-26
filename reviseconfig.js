aa18135ca39927ec724f44daa953b8865493caef
rashiyadhruv <rashiyadhruv@gmail.com>
2023-03-26 19:56:46 +0530

    reviseconfig

---
+++ reviseconfig.js
@@ -0,0 +1,5 @@
+const key =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjBiYjI2MDFiLWY4MjYtNDRiZC04NjA5LTIzMTY4MGZkNmEwNiIsImtleSI6IjUzYWRkemFsIiwiaWF0IjoxNjc2MDAxNTM0fQ.ngdMqTj-gCAvgw1IYYHqCbKM4j43mgux7vFadJaGWjU";
+const collectionId = "9c5a9891-1ca6-456c-88ad-4cbe5ab9d99f";
+
+module.exports = { key, collectionId };
